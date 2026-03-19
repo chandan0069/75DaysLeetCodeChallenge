c4285a100d2e84dd28dbc00e32dbb370ac4c366e
Chandan Chaudhary <162810795+chandan0069@users.noreply.github.com>
2026-03-19 17:16:30 +0530

    Time: 11 ms (91.58%), Space: 12.7 MB (85.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        st = ""
-        for ch in s:
-            ch=ch.lower()
-            if ch.isalnum():
-                st+=ch
-        return st == st[::-1]
+        left, right = 0, len(s) - 1
+        
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
