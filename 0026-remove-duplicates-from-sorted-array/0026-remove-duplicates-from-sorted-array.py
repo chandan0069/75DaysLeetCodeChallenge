465e0d8024b04a6cb3cef0c0485beecc31e9ec23
Chandan Chaudhary <162810795+chandan0069@users.noreply.github.com>
2026-03-20 13:46:49 +0530

    Time: 7 ms (20.25%), Space: 13.8 MB (75.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=len(nums)
+        if not nums:
+            return 0
         i=0
-        for j in range(1,n):
+        for j in range(1,len(nums)):
             if nums[i]!=nums[j]:
                 nums[i+1]=nums[j]
                 i+=1
