2cc609cb5a98d449be4185c4ac6fcc0cb7ba0e0f
Chandan Chaudhary <162810795+chandan0069@users.noreply.github.com>
2026-04-21 13:11:16 +0530

    Time: 3 ms (38.32%), Space: 13.3 MB (15.37%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = [root]
+        front=0
+        result=[]
+        while front<len(queue):
+            level_size=len(queue)-front
+            level=[]
+            for _ in range(level_size):
+                node=queue[front]
+                front+=1
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
