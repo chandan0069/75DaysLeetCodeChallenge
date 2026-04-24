8cf8a0041e5e8da54617e225594b0e152f525002
Chandan Chaudhary <162810795+chandan0069@users.noreply.github.com>
2026-04-24 13:25:52 +0530

    Time: 27 ms (87.1%), Space: 34.4 MB (21.66%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        
+        node.is_end = True   # mark end of word
+
+    def search(self, word: str) -> bool:
+        node = self._traverse(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._traverse(prefix) is not None
+
+    def _traverse(self, string: str):
+        node = self.root
+        
+        for ch in string:
+            if ch not in node.children:
+                return None
+            node = node.children[ch]
+        
+        return node
